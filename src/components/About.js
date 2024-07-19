20e190d9db78e1017cad6a748694739632c07e21
Dave Banwell <dave@davebanwell.com>
2024-07-18 22:04:51 -0400

    completes lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
-}
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I'm currently learning to create web apps with React and JavaScript!</p>
+      <img src={image} alt="I made this" />
+    </div>
+    );
+};
 
 export default About;
